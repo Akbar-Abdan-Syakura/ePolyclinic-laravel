be20748a3e15e1087fa88e5d94fe8bb2d7e2f9c3
Akbar Abdan Syakura <akbarabdansyaku@gmail.com>
2024-03-11 17:18:33 +0700

    create database migration

---
+++ database/migrations/2024_03_11_084713_create_detail_user_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('type_user_id');
+            $table->string('nik')->unique();
+            $table->string('bpjs')->unique();
+            $table->string('contact')->nullable();
+            $table->longText('address')->nullable();
+            $table->longText('photo')->nullable();
+            $table->enum('gender', [1, 2])->nullable();
+            $table->string('age')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_user');
+    }
+}
