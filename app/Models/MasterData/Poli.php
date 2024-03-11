d69232d136a153b7e784ad2e3e7f4216c73fc568
Akbar Abdan Syakura <akbarabdansyaku@gmail.com>
2024-03-11 20:22:50 +0700

    fill all model with fn

--- app/Models/MasterData/Poli.php
+++ app/Models/MasterData/Poli.php
@@ -4,8 +4,28 @@ namespace App\Models\MasterData;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Poli extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+
+    // declare table
+    public $table = 'poli';
+
+    // this field must type date yyyy-mm-dd hh:mm:ss
+    protected $date = [
+        'deleted_at',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'price',
+        'deleted_at',
+        'created_at',
+        'updated_at',
+    ];
 }
--- app/Models/Operational/Appointment.php
+++ app/Models/Operational/Appointment.php
@@ -4,8 +4,33 @@ namespace App\Models\Operational;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Appointment extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+
+    // declare table
+    public $table = 'appointment';
+
+    // this field must type date yyyy-mm-dd hh:mm:ss
+    protected $date = [
+        'deleted_at',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $fillable = [
+        'doctor_id',
+        'user_id',
+        'consultation_id',
+        'level',
+        'date',
+        'time',
+        'status',
+        'deleted_at',
+        'created_at',
+        'updated_at',
+    ];
 }
